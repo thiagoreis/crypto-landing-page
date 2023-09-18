0287985110824ea78931a34e69bb89264150dadf
Thiago Reis <thiagoreis128@gmail.com>
2023-09-17 22:19:48 -0300

    feat: configuração inicial

--- app/rsc/page.tsx
+++ app/rsc/page.tsx
@@ -1,10 +1,10 @@
 // import 'server-only' does not currently
 // work with Vitest
 
-export const metdata = {
-  title: 'App Router',
-}
+export const metadata = {
+  title: "App Router",
+};
 
 export default function Page() {
-  return <h1>App Router</h1>
+  return <h1>App Router</h1>;
 }
